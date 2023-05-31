4cad2c5fae6d3b36e4e89a7e73f2dacea6b6af63
Vivian <vivyan3gmail.com>
2023-05-31 17:12:52 +0300

    to combine profiles with signin option

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,30 +1,38 @@
-import React,{useState} from 'react';
+// import React,{useState} from 'react';
 
-const Signup=()=>{
-    const[name,setName]=useState("");
-    const[admissionNumber,setadmissionNumber]=useState("");
-    const[password,setPassword]=useState("");
-    const collectData=()=>{
-        console.warn(name,admissionNumber,password)
 
-    }
+
+
+
+// const Signup=()=>{
+
+
+//     const[name,setName]=useState("");
+//     const[admissionNumber,setadmissionNumber]=useState("");
+//     const[password,setPassword]=useState("");
+//     const collectData=()=>{
+//         console.warn(name,admissionNumber,password)
+
+//     }
    
 
 
-    return(<div className='register'> <h1>Register </h1>   
-    <input className='inputBox' type= 'text' placeholder='Enter Name'
-    value={name} onChange={(e)=>setName(e.target.value)}/>
+//     return(<div className='register'> <h1>Register </h1>   
+//     <input className='inputBox' type= 'text' placeholder='Enter Name'
+//     value={name} onChange={(e)=>setName(e.target.value)}/>
 
-    <input className='inputBox'type= 'text' placeholder='Enter Admission Number'
-    value={admissionNumber} onChange={(e)=>setadmissionNumber(e.target.value)}/>
+//     <input className='inputBox'type= 'text' placeholder='Enter Admission Number'
+//     value={admissionNumber} onChange={(e)=>setadmissionNumber(e.target.value)}/>
 
-    <input className='inputBox' type= 'password' placeholder='Enter Password'
-    value={password} onChange={(e)=>setPassword(e.target.value)}/>
+//     <input className='inputBox' type= 'password' placeholder='Enter Password'
+//     value={password} onChange={(e)=>setPassword(e.target.value)}/>
 
-    <button  onClick={collectData}    className='appButton'type='button'>Sign Up</button>
+//     <button  onClick={collectData}    className='appButton'type='button'>Sign Up</button>
     
-    </div>
+//     </div>
     
-    )
-}
-export default Signup;
+//     )
+// }
+
+
+// export default Signup;
