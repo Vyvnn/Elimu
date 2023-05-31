7dc9c3ac3a939d5fab7007f67090945022199646
Vivian <vivyan3gmail.com>
2023-05-31 09:16:27 +0300

    set up backend

---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+const express= require ('express');
+const mongoose =require('mongoose');
+
+const app = express();
+const connectDB=()=>{
+        mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        const productSchema=new mongoose.Schema({});
+        const product =mongoose.model("products",productSchema);
+        const db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+        db.once('open', function() {
+            console.log('Connected');
+        });
+}
+connectdb()
+app.listen(3003);
