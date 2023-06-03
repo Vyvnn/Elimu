c326efd6d9dd57bf3a03a31d87e7a3c22cf287b2
Vivian <vivyan3gmail.com>
2023-06-03 11:19:18 +0300

    connect with database

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,40 @@
-const express= require ('express');
-const mongoose =require('mongoose');
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-const connectDB=()=>{
-        mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-        const productSchema=new mongoose.Schema({});
-        const product =mongoose.model("products",productSchema);
-        const db = mongoose.connection;
-        db.on('error', console.error.bind(console, 'connection error:'));
-        db.once('open', function() {
-            console.log('Connected');
-        });
+const connectDB = () => {
+    mongoose.connect('mongodb+srv://vivyan3:burpee6@elimu.lhrbpeb.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+
+//   mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongodb+srv://vivyan3:<password>@elimu.lhrbpeb.mongodb.net/<database-name>
+
+  const productSchema = new mongoose.Schema({});
+  const product = mongoose.model("products", productSchema);
+  const db = mongoose.connection;
+  
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () {
+    console.log('Connected');
+  });
 }
 
-connectdb()
+console.log(process.env.MONGODB_URI);
+connectDB();
+
+//routes
+app.get('/', (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get('/register', (req, res) => {
+  res.render("register.ejs");
+});
+
+app.get('/login', (req, res) => {
+  res.render("login.ejs");
+});
+
 app.listen(3003);
