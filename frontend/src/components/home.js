e4101137a40f893638ce0a80ee47b747c6b26ef5
Vivian <vivyan3gmail.com>
2023-06-16 13:06:28 +0300

    edit button on homepage

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,15 +1,22 @@
 import '../index.css';
 
+import { Link } from "react-router-dom";
+
 
 const Home= () => {
     return ( 
-
+      
 <div>
   <div id="welcome"><p>Welcome to</p>
 
   <h3>Elimu</h3>
 
-  <button id="welcomesignin">SIGN IN</button>
+  <Link to={"/profile"}id="welcomesignin" ><button >SIGN IN 
+     
+     </button></Link>
+  
+
+
   </div>
 
   
