6414f82cde65793bfea09b9d91e587f0a01617d1
Vivian <vivyan3gmail.com>
2023-06-26 22:09:24 +0300

    change homepage pic

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,30 +1,30 @@
 import '../index.css';
 
 import { Link } from "react-router-dom";
+import 'font-awesome/css/font-awesome.min.css';
+
+const Home = () => {
+  return (
+
+    <div className='homepage'>
+      <div id="welcome"><p>Welcome to</p>
+
+        <h3>Elimu</h3>
+
+        <Link to={"/profile"} id="welcomesignin" ><button id="welcomesignin" >SIGN IN
+
+        </button></Link>
 
 
-const Home= () => {
-    return ( 
-      
-<div>
-  <div id="welcome"><p>Welcome to</p>
 
-  <h3>Elimu</h3>
-
-  <Link to={"/profile"}id="welcomesignin" ><button id="welcomesignin" >SIGN IN 
-     
-     </button></Link>
-  
+      </div>
 
 
-  </div>
-
-  
 
 
-</div>
+    </div>
 
-     );
+  );
 }
- 
+
 export default Home;
