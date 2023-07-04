ad4826f38fbda93552b307ca189f27022011afdd
Vivian <vivyan3gmail.com>
2023-07-04 21:22:38 +0300

    to set up server file

--- Backend/index.js
+++ Backend/index.js
@@ -1,57 +1,12 @@
-// require('dotenv').config();
-
-const express = require ('express')
+const express = require('express');
 const app= express();
+app.get('/')
+app.listen(5000,()=>{
 
-app.get('/',(req,res)=>{
-res.json({message: 'Welcome to the Elimu app'})
-
-
-})
-
-app.listen(3003,()=>{
-    console.log('listen to port 3003')
+    console.log('listening to port 5000')
 })
 
 
-
-
-
-
-
-// const mongoose = require('mongoose');
-
-
-
-// const connectDB = () => {
-//  mongoose.connect('mongodb+srv://vivyan3:burpee6@elimu.lhrbpeb.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-// mongodb+srv://vivyan3:<password>@elimu.lhrbpeb.mongodb.net/<database-name>
-
-// const productSchema = new mongoose.Schema({});
-// const product = mongoose.model("products", productSchema);
-// const db = mongoose.connection;
-  
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function () {
-// console.log('Connected');
-// });
-// }
-
-// // console.log(process.env.MONGODB_URI);
-// // connectDB();
-
-
-// // app.use(express.urlencoded({extended:false}))
-// // app.post("/register",async(req,res)=> {
-// //   try {
-
-// //   }catch{
-    
-// //   }
-// // })
-// //routes
 
 
 
