a020c10c496aeadcbdf3cdb314cb4f76ee336869
Vivian <vivyan3gmail.com>
2023-07-06 12:50:50 +0300

    to connect with database

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app= express();
+const mongoose = require ('mongoose');
+const bodyParser=require('body-parser');
+const cookieParser=require('cookie-parser');
+const cors =require('cors')
 
+
+//DB Connection
+mongoose.connect()
 
 //routes
 app.get('/',(req,res)=>{
 res.send('hello')
-
+ 
 })
 
 
