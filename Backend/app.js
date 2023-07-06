a42aa6ad93dd7012644174c2ce5ed2daa008dcb7
Vivian <vivyan3gmail.com>
2023-07-06 13:06:51 +0300

    to define middlewares

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,27 @@ const app= express();
 const mongoose = require ('mongoose');
 const bodyParser=require('body-parser');
 const cookieParser=require('cookie-parser');
-const cors =require('cors')
+const cors =require('cors');
+require("dotenv").config();
 
 
 //DB Connection
-mongoose.connect()
+mongoose.connect(process.env.DB_URI,{
+useNewUrlParser:true,
+useUnifiedTopology:true,
+useCreateIndex:true
+
+}).then(()=>{
+    console.log("DB CONNECTED")
+}).catch(()=>{
+    console.log("UNABLE TO CONNECT TO DB")
+})
+
+
+//use parsing middleware
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors)
 
 //routes
 app.get('/',(req,res)=>{
