958982748e8cb26d0001e6f3f96bd53328cad75a
Vivian <vivyan3gmail.com>
2023-07-20 00:12:01 +0300

    reposition navbar components

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,28 +1,35 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
-    return (  
+    return (
         <nav className="navbar">
-           <Link to='/'> <img src="./images/logo.PNG" alt="logo" className="logo"/></Link>
+         
+         <Link to='/'> <img src="./images/logo.PNG" alt="logo" className="logo" /></Link>
 
-    
             <div className="links">
-           <Link to='/'>Home</Link>
-           <Link to='/profile'>Profile</Link>
-           <Link to='parent/parentmainpage'>Parent</Link>
-           <Link to='student/studentmainpage'>Student</Link>
-           <Link to='teacher/teacherMainPage'>Teacher</Link>
+            
+                <Link to='/'>Home</Link>
+                <Link to='/profile'>Profile</Link>
+                <Link to='parent/parentmainpage'>Parent</Link>
+                <Link to='student/studentmainpage'>Student</Link>
+                <Link to='teacher/teacherMainPage'>Teacher</Link>
 
-            <Link to='/contact_Us'>Contact Us</Link>
+                <Link to='/contact_Us'>Contact Us</Link>
 
 
-                
-                
+              
+
+
+
+
+
+
+
 
             </div>
         </nav>
     );
 }
- 
+
 export default Nav;
--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -11,9 +11,9 @@ const Home = () => {
 
         <h3>Elimu</h3>
         <br />
-<h3>A one stop shop bringing together parents, teachers and students. </h3>
+        <h3>A one stop shop bringing together parents, teachers and students. </h3>
         <Link to={"/profile"} id="welcomesignin" ><button id="welcomesignin" >SIGN IN
-        
+
 
         </button></Link>
 
@@ -21,12 +21,12 @@ const Home = () => {
 
       </div>
 
-<div>
-<p>Elimu is a classroom management tool that enables schools to bring parents, teachers and students into classroom communities so they can track and enhance classroom behavior in real-time.</p>
+      {/* <div>
+        <p>Elimu is a classroom management tool that enables schools to bring parents, teachers and students into classroom communities so they can track and enhance classroom behavior in real-time.</p>
 
-<p>Elimu that helps build amazing classroom and school communities. Teachers give feedback to students for any skill, like “Working hard” and “Being curious.”  Parents view their child’s feedback, message directly with teachers . </p>
+        <p>Elimu that helps build amazing classroom and school communities. Teachers give feedback to students for any skill, like “Working hard” and “Being curious.”  Parents view their child’s feedback, message directly with teachers . </p>
 
-</div>
+      </div> */}
 
 
     </div>
