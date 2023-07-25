816dbef0933db49e362d6b02440fae2aec8a712c
Vivian <vivyan3gmail.com>
2023-07-25 11:26:06 +0300

    to resolve bcrypt error

--- Backend/models/student.js
+++ Backend/models/student.js
@@ -1,63 +1,54 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-  // studentID: {
-  //   type: Number,
-  //   required: true
-  // },
- 
- 
-  subject: {
+  studentName: {
     type: String,
     required: true
   },
-  teacher: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Teacher'
-  },
-  grade: {
+ 
+   grade: {
     type: Number,
     default: null // Set the default value to null
   },
-  remark: {
-    type: String,
-    default: '' // Set the default value to an empty string
-  },
-  // encry_password:{
-  //   type:String,
-  //   required:true
-  //     },
-      salt:String,
-      
-      
-    },{timestamps:true});
-    studentSchema.virtual("password")
-    .set(function(password){
-    _password=password
-    salt= Math.random().toString(26).slice(2)
-    this.encry_password = this.securePassword(password)
-    
-    
-    })
-    .get(function(){
-      return this._password
-    
-    })
-   studentSchema.methods={
-      authenticate:function(plainpassword){
-        return this.securePassword(plainpassword)===this.encry_password
-      },
-      securePassword: function(plainpassword){
-        if(!plainpassword) return"";
-        try{
-          return crypto.createHmac("sha256",this.salt).update(plainpassword).digest('hex')
-        }
-       catch (err){
-        return ""
-      }
-    },
-    }
 
+  password:{
+    type:String,
+    required:true
+      },
+
+      studentNo:{
+        type:Number,
+        required:true
+      }
+      // salt:String, 
+      
+      
+  //   },{timestamps:true});
+  //   studentSchema.virtual("password")
+  //   .set(function(epassword){
+  //   _password=password
+  //   salt= Math.random().toString(26).slice(2)
+  //   this.encry_password = this.securePassword(epassword)
+  
+  //   })
+  //   .get(function(){
+  //     return this._epassword
+    
+  //   })
+  //  studentSchema.methods={
+  //     authenticate:function(plainpassword){
+  //       return this.securePassword(plainpassword)===this.encry_password
+  //     },
+  //     securePassword: function(plainpassword){
+  //       if(!plainpassword) return"";
+  //       try{
+  //         return crypto.createHmac("sha256",this.salt).update(plainpassword).digest('hex')
+  //       }
+  //      catch (err){
+  //       return ""
+       }
+)
+  
 
 const Student = mongoose.model('Student', studentSchema);
 
